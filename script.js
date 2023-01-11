92bf669281ecd7dcc14cd77aa887a306cbdc5405
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-11 12:31:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,38 +3,6 @@ import user from './assets/user.svg'
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
-const copyButtonLabel = "Copy Code";
-
-// use a class selector if available
-let blocks = document.querySelectorAll("div[class="chat"]");
-
-blocks.forEach((block) => {
-  // only add button if browser supports Clipboard API
-  if (navigator.clipboard) {
-    let button = document.createElement("button");
-
-    button.innerText = copyButtonLabel;
-    block.appendChild(button);
-
-    button.addEventListener("click", async () => {
-      await copyCode(block, button);
-    });
-  }
-});
-
-async function copyCode(block, button) {
-  let code = block.querySelector("div[id^="id-"]");
-  let text = code.innerText;
-
-  await navigator.clipboard.writeText(text);
-
-  // visual feedback that task is completed
-  button.innerText = "Code Copied";
-
-  setTimeout(() => {
-    button.innerText = copyButtonLabel;
-  }, 700);
-}
 
 let loadInterval
 
